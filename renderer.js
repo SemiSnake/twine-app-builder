a82c2456b59c7be2d6be312e602a8d24e5bd2e1f
SemiSnake <SemiSnake@users.noreply.github.com>
2024-09-16 09:23:20 +0000

    deploy: a2a29b6a078b2a9170081c90b3d2153acb0c13b3

--- renderer.js
+++ renderer.js
@@ -1,7 +1,7 @@
 const sysInfoBtn = document.getElementById('computerName')
 //document.getElementById('computerName').innerHTML = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
 async function func(){
-  const response = await window.versions.ping()
+  const response = JSON.stringify(await window.versions.ping())
   console.log(response) // prints out 'pong'
   //document.getElementById('computerName').innerHTML= response
 }
