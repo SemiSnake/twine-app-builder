21f9a8f262c2d2959093df5f51563505110597fd
SemiSnake <SemiSnake@users.noreply.github.com>
2024-09-16 08:49:38 +0000

    deploy: 6c07e0f34e352920e5d7a3072c9cd74cfc09136f

--- preload.js
+++ preload.js
@@ -1,7 +1,11 @@
-const { contextBridge } = require('electron')
+const { contextBridge } = require('electron');
+const os = require("os");
 contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron
   // we can also expose variables, not just functions
 })
+contextBridge.exposeInMainWorld('os', {
+    hostname: () => os.hostname,
+  })
