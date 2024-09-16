8ecb412f9e2969ccd8dd2020db38a9ee95aae895
SemiSnake <48889472+SemiSnake@users.noreply.github.com.>
2024-09-16 10:31:02 +0200

    Inline js

--- src/renderer.js
+++
@@ -1,3 +0,0 @@
-const sysInfoBtn = document.getElementById('computerName')
-document.getElementById('computerName').innerHTML = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
-console.log("Js loaded")
