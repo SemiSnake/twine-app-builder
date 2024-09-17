a74d0035339e200d5ff65647903f3afcb9220bdf
SemiSnake <48889472+SemiSnake@users.noreply.github.com.>
2024-09-17 10:24:44 +0200

    asd

--- src/renderer.js
+++ src/renderer.js
@@ -1,4 +1,4 @@
-const sysInfoBtn = document.getElementById('computerName')
+var sysInfoBtn = document.getElementById('computerName')
 //document.getElementById('computerName').innerHTML = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
 async function func(){
   const response = await window.versions.usernameQuery()
