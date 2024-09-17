84622b294190876e18304889ddd6c5cd4e1e3714
SemiSnake <SemiSnake@users.noreply.github.com>
2024-09-17 11:30:38 +0000

    deploy: d96c805556367b03e334a0ca9cd0d5df861dce88

--- renderer.js
+++ renderer.js
@@ -1,7 +1,7 @@
 const spanId = "computerName"
 //document.getElementById('computerName').innerHTML = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
 async function func(){
-  var sysInfoBtn = document.getElementById(id)
+  var sysInfoBtn = document.getElementById(spanId)
   const response = await window.versions.usernameQuery()
   console.log(sysInfoBtn);
   console.log(response);
