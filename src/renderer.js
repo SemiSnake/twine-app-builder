4d23ac2e412a8da45e008d6637805c148dd5b3af
SemiSnake <48889472+SemiSnake@users.noreply.github.com.>
2024-09-19 13:29:12 +0200

    Signed-off-by: SemiSnake <48889472+SemiSnake@users.noreply.github.com.>

--- src/renderer.js
+++ src/renderer.js
@@ -2,8 +2,8 @@ var comNameElements = document.getElementsByClassName("computerName")
 //document.getElementById('computerName').innerHTML = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
 async function func(){
   console.log(comNameElements)
-  for (let i of collection) {
-    var sysInfoBtn = collection[i]
+  for (let i of comNameElements) {
+    var sysInfoBtn = comNameElements[i]
     const response = await window.versions.usernameQuery()
     console.log(sysInfoBtn);
     console.log(response);
