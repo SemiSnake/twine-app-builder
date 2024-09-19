1b40fe5dec64eb52534115673b73a25fe7a2647d
SemiSnake <48889472+SemiSnake@users.noreply.github.com.>
2024-09-19 10:38:08 +0200

    Signed-off-by: SemiSnake <48889472+SemiSnake@users.noreply.github.com.>

--- src/renderer.js
+++ src/renderer.js
@@ -2,11 +2,12 @@ var comNameElements = document.getElementsByClassName("computerName")
 //document.getElementById('computerName').innerHTML = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
 async function func(){
   console.log(comNameElements)
-  console.log(comNameElements.length)
-  var sysInfoBtn = document.getElementById("computerName")
-  const response = await window.versions.usernameQuery()
-  console.log(sysInfoBtn);
-  console.log(response);
-  sysInfoBtn.innerHTML= response
+  for (let i of collection) {
+    var sysInfoBtn = collection[i]
+    const response = await window.versions.usernameQuery()
+    console.log(sysInfoBtn);
+    console.log(response);
+    sysInfoBtn.innerHTML= response
+  }
 }
 func()
